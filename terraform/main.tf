9b1ec073171938e797fd69a729cb4f69d97c400c
Glenn Oppegard <goppegard@pivotal.io>
2019-02-24 14:35:13 -0700

    Add terraform port 80 firewall rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,31 @@ provider "google" {
   project = "dockerclass-232718"
 }
 
+variable "network_name" {
+  type = "string"
+  default = "default"
+}
+
+resource "google_compute_firewall" "http" {
+  name = "http-firewall"
+  network = "${var.network_name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = [
+      "80"]
+  }
+}
+
 resource "google_compute_instance" "studentbox" {
   count = 50
-  name         = "studentbox${count.index}"
+  name = "studentbox${count.index}"
   machine_type = "f1-micro"
-  zone         = "us-central1-a"
+  zone = "us-central1-a"
 
   boot_disk {
     initialize_params {
@@ -15,7 +35,7 @@ resource "google_compute_instance" "studentbox" {
   }
 
   network_interface {
-    network = "default"
+    network = "${var.network_name}"
 
     access_config {
       // Ephemeral IP
@@ -28,4 +48,3 @@ resource "google_compute_instance" "studentbox" {
 
   metadata_startup_script = "sudo /usr/sbin/useradd -G wheel,docker -p $(echo student${count.index} | openssl passwd -1 -stdin) -m -s /bin/bash student${count.index}; sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config; systemctl reload sshd.service"
 }
-
